6de631dda113a89e732b431f1cbc2912419a89bf
SebasSoledad07 <sebastiansg@ufps.edu.co>
2025-07-18 18:38:01 -0400

    feat: añadir componente de saludo y funcionalidad de reproductor
    - Se ha añadido un componente de saludo para mostrar saludos basados en el tiempo.
    - Implementación de un componente Reproductor con funciones de reproducción/pausa y controles de audio.
    - Actualización de los componentes PlaylistItemCard y SideMenuItem para mejorar el estilo y las transiciones.
    - Diseño modificado para incluir el componente Reproductor.
    - Páginas de índice y lista de reproducción mejoradas con nuevos componentes y diseño mejorado.
    - Actualizado package.json para incluir las dependencias necesarias para React y Svelte.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import svelte from '@astrojs/svelte';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
-  ,
-  output: 'server'
+  },
+
+  output: 'server',
+  integrations: [svelte(), react()]
 });
---
+++ src/components/Greeting.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+
+  let greeting: string;
+  if (currentHour < 12) {
+    greeting = "¡Buenos días!";
+  } else if (currentHour < 19) {
+    greeting = "¡Buenas tardes!";
+  } else {
+    greeting = "¡Buenas noches!";
+  }
+</script>
+
+<h1 class="text-3xl font-bold text-white">{greeting}</h1>
